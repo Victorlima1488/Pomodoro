aa84e50ee728981f427a126852300e19aa60e7a4
victorlima1488 <victorlima.softwaredeveloper@gmail.com>
2023-07-16 00:59:09 -0300

    NewPages

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom"
-import { Home } from "./pages/Home"
+import { Home } from "./pages/Home/IndexHome"
 import { History } from "./pages/History"
 import { IndexLayout } from "./layouts/DefaultLayouts/IndexLayout"
 
